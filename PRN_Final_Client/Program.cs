4a9089d05c4b2fc63459388a8a09c4dc1115e12c
hungng1301 <90265514+hungng1301@users.noreply.github.com>
2023-03-27 06:43:40 +0700

    Update Program.cs

--- PRN_Final_Client/Program.cs
+++ PRN_Final_Client/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using PRN_Final_API.Models;
 
@@ -12,6 +13,19 @@ builder.Services.AddSession(options =>
 builder.Services.AddHttpClient();
 builder.Services.AddDbContext<final_projectContext>(opt => opt.UseSqlServer(
             builder.Configuration.GetConnectionString("FinalProject")));
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.Cookie.Name = "AuthCookieName";
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+            options.SlidingExpiration = true;
+        });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Teacher", policy => policy.RequireRole("1"));
+    options.AddPolicy("Student", policy => policy.RequireRole("0"));
+    options.AddPolicy("TeacherOrStudent", policy => policy.RequireRole("1", "0"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +36,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
